8aa3c0cbc76657ab0e05deebaa19fe995f090962
Your Full Name <ehasun0909@gamil.com>
2025-09-07 12:23:31 +0600

    create showing all tress with fetch api and create categoty btn with fetch api

---
+++ js/app.js
@@ -0,0 +1,126 @@
+// All trees data fetch
+let allTressGlobal = []; //Global action
+
+const allTressData = () => {
+  const url = `https://openapi.programming-hero.com/api/plants`;
+
+  fetch(url)
+    .then((res) => {
+      return res.json();
+    })
+
+    .then((data) => {
+      allTressGlobal = data.plants;
+      showAllTress(allTressGlobal);
+    });
+};
+
+allTressData();
+
+// show all trees functionality
+const showAllTress = (allTress) => {
+  const cardSection = document.getElementById("card-section");
+  cardSection.innerHTML = "";
+
+  allTress.forEach((tree) => {
+    const tressDiv = document.createElement("div");
+
+    tressDiv.innerHTML = `
+      <div class="card bg-base-100 w-[290px] shadow-sm p-4">
+          <img src="${tree.image}" alt="Tree" class="h-[200px] w-full object-cover rounded-xl" />
+          <div class="card-body p-0 pt-3">
+              <h5 class="card-title">${tree.name}</h5>
+              <p class="text-[0.8rem]">${tree.description}</p>
+              <div class="flex justify-between mt-2">
+                  <div class="badge badge-soft bg-[#DCFCE7] text-green-700 border-0 py-3 px-4">${tree.category}</div>
+                  <div class="price"><i class="fa-solid fa-bangladeshi-taka-sign"></i> ${tree.price}</div>
+              </div>
+
+              <div class="card-actions mt-2">
+                  <button class="btn bg-[#15803D] w-full rounded-full text-white border-0" id="add-to-cart">
+                      Add To Cart
+                  </button>
+              </div>
+          </div>
+      </div>
+    `;
+
+    cardSection.appendChild(tressDiv);
+  });
+};
+
+// show categories Data Fetch start
+const categoriesData = () => {
+  const url = `https://openapi.programming-hero.com/api/categories`;
+
+  fetch(url)
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      showAllCategoriesBtn(json.categories);
+    });
+};
+
+categoriesData();
+
+function showAllCategoriesBtn(allButtons) {
+  const categoriesBtnSection = document.getElementById("categories-button-sec");
+  categoriesBtnSection.innerHTML = "";
+
+  /* active class system start */
+  let activeBtn = null;
+
+  // common btn class style
+  const baseClass =
+    "cursor-pointer m-1 text-left py-2 px-3 transition duration-300 ease-in-out rounded-lg";
+  const activeClass = "bg-[#15803D] text-white";
+  const hoverClass = "hover:bg-[#15803D] hover:text-white";
+  /* active class system end */
+
+  /* showing all tress button functionality start */
+  const allBtn = document.createElement("button");
+  allBtn.innerText = "All Trees";
+  allBtn.className = `${baseClass} ${hoverClass}`;
+
+  allBtn.addEventListener("click", () => {
+    showAllTress(allTressGlobal);
+
+    if (activeBtn) {
+      activeBtn.className = `${baseClass} ${hoverClass}`;
+      allBtn.className = `${baseClass} ${activeClass}`;
+    }
+
+    activeBtn = allBtn;
+  });
+
+  categoriesBtnSection.appendChild(allBtn);
+  /* showing all tress button functionality end */
+
+  allButtons.forEach((btn) => {
+    const newButtonEntry = document.createElement("button");
+    newButtonEntry.innerText = btn.category_name;
+    newButtonEntry.className =
+      "cursor-pointer m-1 text-left py-2 px-3 transition duration-300 ease-in-out rounded-lg hover:bg-[#15803D] hover:text-white";
+
+    /* category tress filtering functionality start */
+    newButtonEntry.addEventListener("click", () => {
+      const filteredTrees = allTressGlobal.filter((tree) => {
+        return tree.category === btn.category_name;
+      });
+
+      if (activeBtn) {
+        activeBtn.className = `${baseClass} ${hoverClass}`;
+        newButtonEntry.className = `${baseClass} ${activeClass}`;
+      }
+
+      activeBtn = newButtonEntry;
+
+      showAllTress(filteredTrees);
+    });
+    /* category tress filtering functionality end */
+
+    categoriesBtnSection.appendChild(newButtonEntry);
+  });
+}
+// show categories buttons end
